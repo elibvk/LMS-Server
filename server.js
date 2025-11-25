1fcedeb57be54be67cd51a4d7af49bdd68191c93
elibvk <eisoftech.in@gmail.com>
2025-11-25 19:37:40 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -8,13 +8,33 @@ const userAuthRoutes = require('./routes/userAuth');
 
 const app = express();
 
-// Middleware
-app.use(cors({ 
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://resources.vijayonline.in'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'] 
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+// Middleware
+// app.use(cors({ 
+//   origin: process.env.CLIENT_URL || 'http://localhost:5173',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'] 
+// }));
 app.use(express.json());
 
 // Serve uploaded docs as static files
